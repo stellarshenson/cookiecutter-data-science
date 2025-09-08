b45b37dd1eb1e63ddfc0bc421f407041ce56a28f
stellarshenson <konrad.jelen@gmail.com>
2025-09-08 20:25:43 +0000

    updated logging

--- {{ cookiecutter.repo_name }}/src/{{ cookiecutter.module_name }}/config.py
+++ {{ cookiecutter.repo_name }}/src/{{ cookiecutter.module_name }}/config.py
@@ -1,32 +1,39 @@
+import sys
 from pathlib import Path
-
 from dotenv import load_dotenv
 from loguru import logger
 
-# Load environment variables from .env file if it exists
-load_dotenv()
+########### SETUP ###############
 
-# Paths
-PROJ_ROOT = Path(__file__).resolve().parents[2]
-logger.info(f"PROJ_ROOT path is: {PROJ_ROOT}")
-
-DATA_DIR = PROJ_ROOT / "data"
-RAW_DATA_DIR = DATA_DIR / "raw"
-INTERIM_DATA_DIR = DATA_DIR / "interim"
-PROCESSED_DATA_DIR = DATA_DIR / "processed"
-EXTERNAL_DATA_DIR = DATA_DIR / "external"
-
-MODELS_DIR = PROJ_ROOT / "models"
-
-REPORTS_DIR = PROJ_ROOT / "reports"
-FIGURES_DIR = REPORTS_DIR / "figures"
+# set up logger
+logger.remove()
+logger.add(sys.stdout, colorize=True)
 
 # If tqdm is installed, configure loguru with tqdm.write
 # https://github.com/Delgan/loguru/issues/135
 try:
     from tqdm import tqdm
-
-    logger.remove(0)
-    logger.add(lambda msg: tqdm.write(msg, end=""), colorize=True)
+    logger.remove()
+    logger.add(lambda msg: tqdm.write(msg, end="", file=sys.stdout), colorize=True)
 except ModuleNotFoundError:
     pass
+
+########## VARIABLES ############
+
+# Load environment variables from .env file if it exists
+load_dotenv()
+
+# paths
+PROJ_ROOT = Path(__file__).resolve().parents[2]
+DATA_DIR = PROJ_ROOT / "data"
+RAW_DATA_DIR = DATA_DIR / "raw"
+INTERIM_DATA_DIR = DATA_DIR / "interim"
+PROCESSED_DATA_DIR = DATA_DIR / "processed"
+EXTERNAL_DATA_DIR = DATA_DIR / "external"
+MODELS_DIR = PROJ_ROOT / "models"
+REPORTS_DIR = PROJ_ROOT / "reports"
+FIGURES_DIR = REPORTS_DIR / "figures"
+
+# log current root dir
+logger.info(f"PROJ_ROOT path is: {PROJ_ROOT}")
+
